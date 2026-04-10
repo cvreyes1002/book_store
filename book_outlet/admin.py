4e8c218ea9781bafbbc81afd16860e51422ff2bc
C.Reyes <cvreyes@hotmail.com>
2026-04-10 15:20:54 +0800

    No. 108

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 
 # Register your models here.
+
 
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
@@ -9,4 +10,6 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("author", "rating",)
     list_display = ("title", "author",)
 
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
